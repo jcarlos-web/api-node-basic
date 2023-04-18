31e892a2b5dfb7efe51ee69906bac95218e7979a
JCarlos <jcarlos.mendozas.s91@gmail.com>
2023-04-18 06:38:21 -0600

    Vector estudiantes

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const app = express();
 // Utilizar librerías JSON
 app.use(express.json());
 
+
+
 // Vector de estudiantes
 const students = [
     {id: 1, name: 'JCarlos', age: 20, enroll: true},
@@ -19,56 +21,6 @@ const students = [
     {id: 3, name: 'Salomón', age: 2, enroll: false}
 ];
 
-
-// Método que imprime sección raiz
-app.get('/', (req, res)=> {
-    res.send('Welcome API!');
-});
-
-
-// Método que imprime estudiantes en formato JSON
-app.get('/api/students', (req, res) => {
-    res.send(students);
-});
-
-
-// Método que busca un estudiante y lo imprime 
-app.get('/api/students/:id', (req, res) => {
-    const student = students.find(c => c.id === parseInt(req.params.id));
-    if (!student) return res.status(404).send('No se encontro el estudiante');
-    else res.send(student);
-});
-
-
-// Método que agrega elementos al vector estudiantes 
-app.post('/api/students', (req, res) => {
-    const student = {
-        id: students.length + 1,
-        name: req.body.name,
-        age: parseInt(req.body.age),
-        enroll: (req.body.enroll === 'true')
-    };
-
-    students.push(student);
-    res.send(student);
-});
-
-
-
-// Método que elimina un elemento en vector estudiantes
-app.delete('/api/students/:id', (req, res) => {
-
-    // Buscar estudiante
-    const student = students.find(c => c.id === parseInt(req.params.id));
-    // Si no existe 
-    if (!student) return res.status(404).send('Estudiante no encontrado');
-    // Indice del estudiante
-    const index = students.indexOf(students);
-    // Borrar mediante comando splice
-    students.splice(index, 1);
-    // Imprimir 
-    res.send(student);
-});
 
 
 // Configurar puerto a escuchar
